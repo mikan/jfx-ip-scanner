608947230140621a82fbc0a993301ce7393335d6
Yutaka Kato <mikan@aomikan.org>
2021-02-27 03:25:45 +0900

    #1 create resource bundle of English and Japanese

--- src/main/kotlin/com/github/mikan/ipscan/ui/App.kt
+++ src/main/kotlin/com/github/mikan/ipscan/ui/App.kt
@@ -7,15 +7,18 @@ import javafx.fxml.FXMLLoader
 import javafx.scene.Scene
 import javafx.scene.control.Alert
 import javafx.stage.Stage
+import java.util.ResourceBundle
 
 class App : Application() {
     override fun start(stage: Stage) {
-        stage.title = "JFX IP Scanner v" + Version.get()
-        stage.scene = Scene(FXMLLoader.load(this::class.java.getResource("/fxml/home.fxml")))
+        stage.title = messages.getString("app.name") + " v" + Version.get()
+        stage.scene = Scene(FXMLLoader.load(this::class.java.getResource("/fxml/home.fxml"), messages))
         stage.show()
     }
 
     companion object {
+        internal val messages: ResourceBundle = ResourceBundle.getBundle("messages")
+
         fun errorDialog(contentText: String) {
             val alert = Alert(Alert.AlertType.ERROR)
             alert.contentText = contentText
@@ -23,9 +26,9 @@ class App : Application() {
         }
 
         fun startCommandStage(command: NetworkCommand, vararg params: String) {
-            val loader = FXMLLoader(this::class.java.getResource("/fxml/command.fxml"))
+            val loader = FXMLLoader(this::class.java.getResource("/fxml/command.fxml"), messages)
             val stage = Stage()
-            stage.title = "JFX IP Scanner"
+            stage.title = messages.getString("app.name")
             stage.scene = Scene(loader.load())
             val controller = loader.getController<CommandController>()
             controller.okButton.setOnAction {
